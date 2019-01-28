4441adcefd937d431620da6ec89f211d48191842
himanshuawapushp <47097405+himanshuawapushp@users.noreply.github.com>
2019-01-28 20:24:47 +0530

    21
    21

---
+++ Personal.java
@@ -0,0 +1,20 @@
+package com.capg.lab21;
+
+public class Personal {
+
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Personal Details");
+		System.out.println("_________");
+		System.out.println("First name: Himanshu");
+		System.out.println("Last Name: Mishra");
+		System.out.println("Gender: M");
+		System.out.println("Age:20");
+		System.out.println("Weight:75");
+		
+
+	}
+
+}
