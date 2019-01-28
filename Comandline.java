37bcd84d764b5cc3b5d3e7b7a46a5a5a8caaabab
himanshuawapushp <47097405+himanshuawapushp@users.noreply.github.com>
2019-01-28 22:19:40 +0530

    22

---
+++ Comandline.java
@@ -0,0 +1,25 @@
+package com.capg.lab22;
+import java.util.*;
+
+public class Comandline {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Enter the number\n"+ args[0]);
+		if (Integer.parseInt(args[0])>=0)
+		{
+			System.out.println("Number is positive");
+		}
+		else
+		{
+			System.out.println("Number is positive");
+		}
+		
+		
+		
+		
+
+	}
+
+}
