927b9ec1557f9c5218fd033d57e548b8bcca886d
himanshuawapushp <47097405+himanshuawapushp@users.noreply.github.com>
2019-01-28 22:21:56 +0530

    24

--- Main.java
+++ Main.java
@@ -1,31 +1,25 @@
-package com.capg.lab25;
-
-
-
-enum Gender {
-	f,m;
-}
+package com.capg.lab24;
 
 public class Main {
-	
 
-	public static void main(String[] args) {
-		Enumber per=new Enumber("Himanshu","mishra",Gender.m,"9598526348");
-	System.out.println("First Name"+ per.getFirstName());
-	System.out.println("Last Name"+ per.getLastName());
-	System.out.println("Gender"+ per.getGender());
-	System.out.println("Mobile number"+per.getMobileNo());
-	per.setFirstName("vaibhav");
-	per.setLastName("shukla");
-	
-	per.setMobileNo("8858783007");
-	per.setGender(Gender.f);
-	System.out.println("First"+ per.getFirstName());
-	System.out.println("Last"+per.getLastName());
-	System.out.println("Gender"+per.getGender());
-	System.out.println("Mobile"+per.getMobileNo());
-	
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
+		Phone per=new Phone("Himanshu","mishra","g","9598526348");
+		System.out.println("First Name"+ per.getFirstName());
+		System.out.println("Last Name"+ per.getLastName());
+		System.out.println("Gender"+ per.getGender());
+		System.out.println("Mobile number"+per.getMobileNo());
+		per.setFirstName("vaibhav");
+		per.setLastName("shukla");
+		per.setMobileNo("8858783007");
+		per.setGender("F");
+		System.out.println("First"+ per.getFirstName());
+		System.out.println("Last"+per.getLastName());
+		System.out.println("Gender"+per.getGender());
+		System.out.println("Mobile"+per.getMobileNo());
+		
+		
 
 	}
 
